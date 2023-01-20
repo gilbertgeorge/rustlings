3618fa456d84d2f7580c3cc08a886bd32def2c9b
gilbertgeorge <george@grgilbert.com>
2023-01-20 12:19:19 -0500

    worked on variables

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -14,6 +14,9 @@ fn main() {
     println!("Base 16 (hexadecimal): {:x}", 69420); //10f2c
     println!("Base 16 (hexadecimal): {:X}", 69420); //10F2C
 
+    // named formatted
+    println!("{type}{number:>20X}",type="Base 16:", number=69420);
+
     // You can right-justify text with a specified width. This will
     // output "    1". (Four white spaces and a "1", for a total width of 5.)
     println!("{number:>5}", number=10);
@@ -22,13 +25,13 @@ fn main() {
     // You can pad numbers with extra zeroes,
     //and left-adjust by flipping the sign. This will output "10000".
     println!("{number:0<5}", number=20);
-    println!("{name:<10}:{otherName}", name="George", otherName="test");
+    println!("{name:.<10}:{otherName}", name="George", otherName="test");
 
     // You can use named arguments in the format specifier by appending a `$`
     println!("{number:0>width$}", number=30, width=5);
 
+    println!("{}", test="Debug");
+    println!("{:?}", test="Debug");
+
 
 }
-
-
-// I AM NOT DONE
--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -2,9 +2,11 @@
 // Make me compile!
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
+    let mut y = 10;
+    println!("mutable y has the value {}", y);
+    y = 11;
+    println!("mutable y now has the value {}", y);
 }
